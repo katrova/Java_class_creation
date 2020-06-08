037597e5c7809bd2f35351385dff93984e256909
Kate <Kate@MacBook-Pro-Catherine.local>
2020-06-08 22:45:54 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,26 @@
+package com.company;
+
+/*
+ * Classname : Java class creation
+ *
+ * 7 June 2020
+ *
+ * Copyright Vasylkivska Kateryna KNTEU
+ *
+ * Module 2 task 1
+ *
+ * 15. Rectangular triangle.
+ *
+ * 1. Constructor.
+   2. Getters/Setters.
+   3. 5 methods.
+   4. Override toString() method
+   5. Override hash() and equals() methods.
+ */
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
