a103bd82e3b758610214f7aa304612f7590c5072
Kate <kate@macbook-pro-catherine.local>
2020-06-10 18:33:20 +0300

    Constructors

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,26 +1,22 @@
 package com.company;
 
 /*
- * Classname : Java class creation
+ * Classname : Main
  *
  * 7 June 2020
  *
  * Copyright Vasylkivska Kateryna KNTEU
  *
- * Module 2 task 1
+ * Module 2
  *
- * 15. Rectangular triangle.
- *
- * 1. Constructor.
-   2. Getters/Setters.
-   3. 5 methods.
-   4. Override toString() method
-   5. Override hash() and equals() methods.
  */
 
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        RectangularTriangle triangle = new RectangularTriangle(3,4);
+        System.out.println(triangle);
+
     }
 }
